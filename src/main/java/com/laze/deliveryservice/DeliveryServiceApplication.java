13cb6033859986a9a718309d22904c3c26aebe76
L-a-z-e <yysi8771@gmail.com>
2024-03-26 00:22:40 +0900

    first commit & push

--- src/main/java/com/laze/deliveryservice/DeliveryServiceApplication.java
+++ src/main/java/com/laze/deliveryservice/DeliveryServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DeliveryServiceApplication {
-
+// 생성
     public static void main(String[] args) {
         SpringApplication.run(DeliveryServiceApplication.class, args);
     }
